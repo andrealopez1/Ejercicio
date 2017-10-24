e32e2bf29f717679b6fa41e4c250f55a26a99540
andrealopez1 <32958471+andrealopez1@users.noreply.github.com>
2017-10-24 07:26:12 -0500

    Update inicial.php

--- inicial.php
+++ inicial.php
@@ -1,4 +1,19 @@
 <?php
-$NombrePersona="Pepito";
-echo "Hola ".NombrePersona
+$NombePersona="Pepito";
+//error
+echo "Hola ".$NombePersona
+  function ProbarBD()
+{
+if ($OK)
+  return 0;
+if(!$OK)
+  return 1;
+}
+  
+  
+  ProbarBD();
+  
+return 1;
+  
+  
 ?>
